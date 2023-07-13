390811a3df9a150dfc8c63a11f699af834f2e884
sharanyaparimanoharan <sharanyaparimanoharan@localhost>
2023-07-13 22:10:53 +0530

    First Commit

---
+++ Test-Automation-Assignmnet-main/S17441Test_Automation/Test_Automation_Assignment/src/test/java/Common.java
@@ -0,0 +1,8 @@
+
+public class Common {
+	public static final String WEB_URL = "https://demo.actitime.com/login.do";
+    public static final String CHROME_ARGUMENT =  "--remote-allow-origins=*";
+    public static final String Password =  "123";
+    public static final String UserName =  "admin";
+
+}
